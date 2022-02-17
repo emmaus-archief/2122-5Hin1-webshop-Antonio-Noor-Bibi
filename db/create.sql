9f593fed17fc1ce938ba0d98d467813eefed2b79
Noor Bouharrou <74540136+noorbouharrou@users.noreply.github.com>
2022-02-17 21:50:33 +0000

    namen veranderd

--- db/create.sql
+++ db/create.sql
@@ -18,7 +18,6 @@ CREATE TABLE info (
   prijs NUMERIC(10, 2)
 );
 
-
 --
 -- populate with data
 --
@@ -28,10 +27,10 @@ CREATE TABLE info (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('French Film', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5);
-insert into products (name, description, code, price) values ('Post Mortem', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
-insert into products (name, description, code, price) values ('Scarlet and the Black, The', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('Aquí llega Condemor, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
+insert into products (name, description, code, price) values ('kylie', 'a', '0000001', 10.5);
+insert into products (name, description, code, price) values ('chanel', 'b', '0000002', 11);
+insert into products (name, description, code, price) values ('fenty', 'c', '0000003', 13.5);
+insert into products (name, description, code, price) values ('huda beauty', 'd', '0000004', 13.5);
+insert into products (name, description, code, price) values ('too faced', 'e', '0000005', 14);
+insert into products (name, description, code, price) values ('anasatasia beverly hills', 'f', '0000006', 14);
 
