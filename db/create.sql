6c16be1e51e1e7749f10d9e25a9efbcfb2ac65e2
Noor Bouharrou <74540136+noorbouharrou@users.noreply.github.com>
2022-03-04 14:09:17 +0000

    database compleet aangepast

--- db/create.sql
+++ db/create.sql
@@ -5,10 +5,13 @@
 CREATE TABLE products (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   code VARCHAR(7),
-  brand VARCHAR(255),
+	name VARCHAR(255),
+  brand_id VARCHAR(255),
+	voorraad_id VARCHAR(255),
   color VARCHAR(255),
+	categorie_id VARCHAR(255),
   description TEXT,
-  price NUMERIC(10, 2),
+  price NUMERIC(10, 2)
   );
 
   CREATE TABLE brand (
@@ -16,7 +19,7 @@ CREATE TABLE products (
   name VARCHAR(255)
   );
 
-  CREATE TABLE color (
+  CREATE TABLE voorraad (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(255)
   );
@@ -35,13 +38,37 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, code, color, brand, price) values ('kylie', '0000001', 'pink', 'kylie cosmetics',  10.5);
-insert into products (name, code, color, brand, price) values ('chanel', '0000002', 'pink', 'chanel',  12.5);
-insert into products (name, code, color, brand, price) values ('fenty', '0000003', 'pink', 'fenty cosmetics',  12.5);
-insert into products (name, code, color, brand, price) values ('huda beauty', '0000004', 'beige', 'huda', 13.5);
-insert into products (name, code, color, brand, price) values ('too faced', '0000005', 'brown', 'too faced', 14);
-insert into products (name, code, color, brand, price) values ('anasatasia beverly hills', '0000006', 'blue', 'anastasia', 15);
-insert into products (name, code, color, brand, price) values ('kkw', '0000007', 'yellow', 'kim', 16);
-insert into products (name, code, color, brand, price) values ('idk', '0000008', 'idc', 'boeie', 18);
+insert into products (name, description, color, code, price, brand_id, voorraad_id, categorie_id) values ('Lip-kit', '-', 'pink', '0000001', 29.99, 1, 1, 2);
 
+	
+
+insert into brand (name) values ('Kylie Cosmetics');
+insert into brand (name) values ('Fenty Beauty');
+insert into brand (name) values ('Too Faced');
+insert into brand (name) values ('Huda Beauty');
+insert into brand (name) values ('Anastasia Beverly Hills');
+
+insert into voorraad (name) values ('op voorraad');
+insert into voorraad (name) values ('beperkte voorraad');
+insert into voorraad (name) values ('uitverkocht');
+
+insert into categorie (name) values ('gezicht');
+insert into categorie (name) values ('lippen');
+insert into categorie (name) values ('ogen');
+insert into categorie (name) values ('wenkbrauwen');
+insert into categorie (name) values ('wangen');
+insert into categorie (name) values ('extras');
+
+
+SELECT * FROM products
+JOIN brand ON 
+brand.name = brand.id;
+
+SELECT * FROM products
+JOIN voorraad ON 
+voorraad.name = voorraad.id;
+
+SELECT * FROM products
+JOIN categorie ON 
+categorie.name = categorie.id;
 
